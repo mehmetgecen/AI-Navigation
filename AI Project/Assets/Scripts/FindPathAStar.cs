991e3c5a838c488f7bd1d72f827ac77577e5e661
mehmetgecen <mehmetgecen06@gmail.com>
2023-04-27 16:13:43 +0300

    Maze and Node Generation

--- AI Project/Assets/Scripts/FindPathAStar.cs
+++ AI Project/Assets/Scripts/FindPathAStar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -56,6 +57,59 @@ public class FindPathAStar : MonoBehaviour
 
     private PathMarker goalNode;
     private PathMarker startNode;
-    
-    
+    private PathMarker lastPos;
+
+    private bool isDone = false;
+
+    void RemoveAllMarkers()
+    {
+        GameObject[] markers = GameObject.FindGameObjectsWithTag("marker");
+
+        foreach (GameObject marker in markers)
+        {
+            Destroy(marker);
+        }
+        
+    }
+
+    void BeginSearch()
+    {
+        isDone = false;
+        RemoveAllMarkers();
+
+        List<MapLocation> locations = new List<MapLocation>();
+
+        for (int z = 1; z < maze.depth -1 ; z++)
+        {
+            for (int x = 1; x < maze.width -1; x++)
+            {
+                if (maze.map[x,z] != 1)
+                {
+                    locations.Add(new MapLocation(x,z));
+                }
+            }
+        }
+        
+        locations.Shuffle();
+
+        Vector3 startLocation = new Vector3(locations[0].x * maze.scale, 0, locations[0].z * maze.scale);
+        
+        startNode = new PathMarker(new MapLocation(locations[0].x, locations[0].z), 0, 0,0,
+            Instantiate(start, startLocation, Quaternion.identity),null);
+        
+        Vector3 goalLocation = new Vector3(locations[1].x * maze.scale, 0, locations[1].z * maze.scale);
+        
+        goalNode = new PathMarker(new MapLocation(locations[1].x, locations[1].z), 0, 0,0,
+            Instantiate(end, goalLocation, Quaternion.identity),null);
+
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            BeginSearch();
+        }
+    }
 }
